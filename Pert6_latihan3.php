02c6b9a6f0c2c6b3e2a5043d4523eee380caeb65
lutfiseptian <61748642+lutfiseptian@users.noreply.github.com>
2021-04-03 15:11:04 +0700

    Add files via upload

---
+++ Pert6_latihan3.php
@@ -0,0 +1,11 @@
+<?php
+$UsiaSiswa["Lutfi"] = "18";
+$UsiaSiswa["Dani"] = "17";
+$UsiaSiswa["Raja"] = "16";
+$UsiaSiswa["Rama"] = "19";
+$UsiaSiswa["Vedran"] = "17";
+foreach($UsiaSiswa as $Nama => $umur)
+{
+echo "Nama Karyawan: $Nama, Usia: $umur"." th <br>";
+}
+?>
